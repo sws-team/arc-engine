d5cf032b4ba4b8b51f860907e4a2be13b2fb932b
Marat <Warkl.78@gmail.com>
2019-12-17 22:19:18 +0300

    no message

--- Game/Level/tower.cpp
+++ Game/Level/tower.cpp
@@ -221,7 +221,7 @@ const int PowerTower::COST_OFFSET = 10;
 const TowerStats PowerTower::STATS = TowerStats		(0,		5000,	5,		0,		60);
 const TowerStats BaseTower::STATS = TowerStats		(5,		450,	4,		20,		50);
 const TowerStats FreezeTower::STATS = TowerStats	(5,		350,	6,		10,		80);
-const TowerStats RocketTower::STATS = TowerStats	(55,	4000,	12,		5,		150);
+const TowerStats RocketTower::STATS = TowerStats	(35,	5000,	12,		5,		150);
 const TowerStats LaserTower::STATS = TowerStats		(15,	200,	6,		0,		180);
 const TowerStats ImprovedTower::STATS = TowerStats	(20,	150,	7,		50,		250);
 
