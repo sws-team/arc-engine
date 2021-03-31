11c9ff1cd19b02eb901364752ddeb9977246745e
Marat T <W40k@mail.ru>
2021-03-31 22:57:49 +0300

    include fix

--- Resources/resources.cmake
+++ Resources/resources.cmake
@@ -2,3 +2,4 @@ set(RESOURCES_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/gameresource.cpp
     )
 include_directories(${CMAKE_CURRENT_LIST_DIR})
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../3rdParty/Base64)
